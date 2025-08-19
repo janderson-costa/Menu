3977d0c3ad2306b782beb45cbd004b394cdebad8
janderson-costa <jandersonfcosta@outlook.com>
2025-08-18 21:29:51 -0300

    ---

--- Menu.js
+++ Menu.js
@@ -3,18 +3,28 @@
 	Descrição: Menu de contexto simples.
 */
 
-const __defaultOptions = {
+const __itemDefaultOptions = {
+	icon: null, // HTMLElement (opcional)
+	id: null, // string (opcional)
+	name: null, // string
+	description: null, // string (opcional)
+	hidden: false, // boolean (opcional)
+	onClick: null, // function
+	divider: false, // boolean
+
+	// Funções geradas do item
+	getIcon: null, // function
+	setIcon: null, // function
+	show: null, // function
+	hide: null, // function
+};
+const __menuDefaultOptions = {
 	trigger: null, // HTMLElement - Ex.: button | a | div
-	items: [], /* item: {
-		icon: HTMLElement, (opcional)
-		id: string, (opcional)
-		name: string,
-		description: string, (opcional)
-		onClick: function
-	}*/
+	items: [], // __itemDefaultOptions[]
 	position: 'left', // 'left' | 'right' | 'top left' | 'top right'
 	top: 0, // Ajuste de posição vertical (opcional)
 	left: 0, // Ajuste de posição horizontal (opcional)
+	maxHeight: null, // Altura máxima (px) do menu (opcional)
 	onShow: null,
 	onHide: null,
 };
@@ -27,10 +37,10 @@ window.addEventListener('resize', event => {
 	destroy(__menu);
 });
 
-export default function Menu(defaultOptions) {
-	defaultOptions = {
-		...__defaultOptions,
-		...defaultOptions,
+export default function Menu(menuDefaultOptions) {
+	menuDefaultOptions = {
+		...__menuDefaultOptions,
+		...menuDefaultOptions,
 	};
 
 	let $menu;
@@ -38,7 +48,7 @@ export default function Menu(defaultOptions) {
 	let _classInvisible = '';
 
 	const _context = {
-		options: defaultOptions,
+		options: menuDefaultOptions,
 		element: null,
 		show,
 		hide,
@@ -47,15 +57,22 @@ export default function Menu(defaultOptions) {
 	return _context;
 
 	function create() {
+		menuDefaultOptions.items.forEach(item => {
+			item = {
+				...__itemDefaultOptions,
+				...item,
+			};
+		});
+
 		const $menu = document.createElement('div');
 
 		$menu.className = 'ctx-menu';
-		$menu.innerHTML = /*html*/`${defaultOptions.items.map(item => {
+		$menu.innerHTML = /*html*/`${menuDefaultOptions.items.map(item => {
 			if (item.divider) {
 				return /*html*/`<div class="ctx-divider"></div>`;
 			} else {
 				return /*html*/`
-					<div class="ctx-item">
+					<div class="ctx-item" name="${item.name}">
 						<div class="ctx-icon"></div>
 						<div class="ctx-text">
 							<div class="ctx-name">${item.name}</div>
@@ -66,28 +83,33 @@ export default function Menu(defaultOptions) {
 			}
 		}).join('')}`;
 
+		if (menuDefaultOptions.maxHeight) {
+			let unit = typeof menuDefaultOptions.maxHeight == 'number' ? 'px' : '';
+
+			$menu.style.maxHeight = menuDefaultOptions.maxHeight + unit;
+		}
+
 		// Itens
 		$menu.querySelectorAll(':scope > div').forEach(($item, index) => {
-			const item = defaultOptions.items[index];
-			const icon = item.icon;
-
-			$item.data = item;
-			item.element = $item;
-
-			// Ícone
+			const item = menuDefaultOptions.items[index];
 			const $icon = $item.querySelector('.ctx-icon');
 
-			if (icon != undefined && icon != null) {
-				if (typeof icon == 'string')
-					$icon.innerHTML = icon;
-				else if (icon instanceof HTMLElement)
-					$icon.appendChild(icon);
-			} else {
-				$icon.style.display = 'none';
-			}
+			item.element = $item;
+			$item.data = item;
 
-			// Evento
-			if (item.divider == undefined) {
+			// Funções do item
+			item.getIcon = () => $icon;
+			item.setIcon = stringOrElement => setIcon($icon, stringOrElement);
+			item.show = (show = true) => $item.classList[show ? 'remove' : 'add']('hidden');
+			item.hide = (hide = true) => item.show(!hide);
+
+			if (item.hidden)
+				item.hide();
+			
+			if (!item.divider) {
+				setIcon($icon, item.icon);
+
+				// Evento
 				$item.addEventListener('click', event => {
 					hide();
 
@@ -103,11 +125,28 @@ export default function Menu(defaultOptions) {
 		return $menu;
 	}
 
+	function setIcon($icon, stringOrElement) {
+		if (
+			typeof stringOrElement == 'undefined' ||
+			stringOrElement == null
+		) {
+			$icon.classList.add('hidden');
+			return;
+		}
+
+		$icon.innerHTML = '';
+
+		if (typeof stringOrElement == 'string')
+			$icon.innerHTML = stringOrElement;
+		else if (stringOrElement instanceof HTMLElement)
+			$icon.appendChild(stringOrElement);
+	}
+
 	function show(options = {}) {
 		destroy($menu);
 
 		options = {
-			...defaultOptions,
+			...menuDefaultOptions,
 			...options,
 		};
 
@@ -175,8 +214,8 @@ export default function Menu(defaultOptions) {
 		$menu.classList.remove(_classVisible);
 		$menu.classList.add(_classInvisible);
 
-		if (defaultOptions.onHide)
-			defaultOptions.onHide(_context);
+		if (menuDefaultOptions.onHide)
+			menuDefaultOptions.onHide(_context);
 
 		setTimeout(() => destroy($menu), 200);
 
